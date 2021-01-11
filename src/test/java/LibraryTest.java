609cc1e1cf7d9ff2a4cc3acb1058cb3d80a55dc1
Steve Turley <steve@turleylabs.com>
2021-01-11 11:33:23 -0600

    Fixes

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LibraryTest {
+    @Test
+    public void testIt() {
+        assertEquals(0, 1);
+    }
+}
